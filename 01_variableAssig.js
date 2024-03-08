d450a6ab838f3329633c09593ebefa615ed9721f
Manisha3055 <147181703+Manisha3055@users.noreply.github.com>
2024-03-08 17:23:33 +0530

    Variable Assignment 1
    Variable Assignment 1

--- 01_variableAssig.js
+++ 01_variableAssig.js
@@ -1,1 +1,32 @@
-var myFullName;
+console.log("=== Assignment 1====");
+var myFullName; 
+console.log("My Full Name variable value before Initialization:", myFullName);
+myFullName="Manisha Mane";
+console.log("My Full Name variable value after Initialization:", myFullName);
+myFullName="Manisha Prabhakar Mane";
+console.log("My Full Name variable value after updation:", myFullName);
+
+console.log("=== Assignment 2 ====");
+
+var myLove;
+myLove = "I love only JS";
+console.log(myLove);
+console.log("=== Assignment 3 ====");
+var firstName;
+var lastName;
+var Age;
+var Address;
+var Gender;
+var isMarried;
+firstName = "Manisha";
+lastName = "Mane";
+Age = 23;
+Address = "Pune";
+Gender = "Female";
+isMarried= "No";
+console.log("First name:", firstName);
+console.log("Last name:", lastName);
+console.log("My Age:", Age );
+console.log("My Address:", Address);
+console.log("Gender:", Gender);
+console.log("Married Status:", isMarried);
