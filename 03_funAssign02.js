1435459c6c9b7f736d5370e17d84fb6fda61fce4
Manisha3055 <147181703+Manisha3055@users.noreply.github.com>
2024-03-11 23:29:09 +0530

    Code for function with diff Argu
    Code for function with diff Argu

---
+++ 03_funAssign02.js
@@ -0,0 +1,12 @@
+function bankDetails(bankName, accountNum, location, pinCode) {
+    console.log("Bank Name:", bankName);
+    console.log("Account Number:", accountNum);
+    console.log("Location:", location);
+    console.log("PIN Code:", pinCode);
+
+}
+
+// Invoke the function with different arguments
+bankDetails("CITI Bank", 3456782345, 'Pune', 431202);
+bankDetails("Axis Bank", 7856782345, 'Mumbai', 441202);
+bankDetails("HDFC Bank", 8956782345, 'Pune', 631202);
